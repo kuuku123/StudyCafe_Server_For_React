cb5213eabeeaa8b87a94265c6ad8d0bce08fbc94
kuuku123 <dohyun0047@gmail.com>
2025-01-04 17:02:59 +0900

    done: 1) moved test container postgres to mysql
    todo:
    1) need to configure redis testcontainer

--- src/test/java/com/StudyCafe_R/infra/AbstractContainerBaseTest.java
+++ src/test/java/com/StudyCafe_R/infra/AbstractContainerBaseTest.java
@@ -1,12 +1,29 @@
 package com.StudyCafe_R.infra;
 
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.MySQLContainer;
 
 public abstract class AbstractContainerBaseTest {
 
-    static final PostgreSQLContainer POSTGRE_SQL_CONTAINER;
-    static  {
-        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer(PostgreSQLContainer.IMAGE);
-        POSTGRE_SQL_CONTAINER.start();
-    }
+//    private static final String REDIS_IMAGE = "redis:7.0.8-alpine";
+//    private static final int REDIS_PORT = 16379;
+//    private static final GenericContainer REDIS_CONTAINER;
+//
+//    static {
+//        REDIS_CONTAINER = new GenericContainer(REDIS_IMAGE)
+//                .withExposedPorts(REDIS_PORT)
+//                .withReuse(true);
+//        REDIS_CONTAINER.start();
+//    }
+//
+//    @DynamicPropertySource
+//    private static void registerRedisProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+//        registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(REDIS_PORT)
+//                .toString());
+//    }
 }
+
