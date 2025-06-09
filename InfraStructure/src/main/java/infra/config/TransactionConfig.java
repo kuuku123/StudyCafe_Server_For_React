e25bab295d013f0bf5fbc6b77452cfe134a95f12
kuuku123 <dohyun0047@gmail.com>
2025-06-09 18:45:47 +0900

    done: 1) add TransactionConfig for read only template
    todo:
    0) need to test spring integration end to end
    
    1) AccountModifierUseCase now doesn't throw exception but call presentError to return proper json object
    
    2) need to have Tag and Zone as aggregator and entity for each domain and infrastructure module

---
+++ InfraStructure/src/main/java/infra/config/TransactionConfig.java
@@ -0,0 +1,34 @@
+package infra.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+/*
+    References:
+    ----------
+    1. Programmatic transaction management, Spring Docs: https://docs.spring.io/spring-framework/reference/data-access/transaction/programmatic.html
+ */
+
+@Configuration
+public class TransactionConfig {
+
+    @Bean
+    @Primary
+    public TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
+        // default settings for transaction propagation and isolation
+        return new TransactionTemplate(transactionManager);
+    }
+
+    @Bean
+    @Qualifier("read-only")
+    public TransactionTemplate readOnlyTransactionTemplate(PlatformTransactionManager transactionManager) {
+        TransactionTemplate template = new TransactionTemplate(transactionManager);
+        template.setReadOnly(true);
+        return template;
+    }
+
+}
--- InfraStructure/src/main/java/infra/config/UseCaseConfig.java
+++ InfraStructure/src/main/java/infra/config/UseCaseConfig.java
@@ -1,19 +1,13 @@
 package infra.config;
 
 import com.StudyCafe_R.usecase.account.AccountModifierPresenterOutputPort;
-import infra.adapter.presenter.CommonRestPresenter;
-import infra.adapter.presenter.account.AccountPresenter;
 import com.StudyCafe_R.usecase.account.AccountModifierInputPort;
 import com.StudyCafe_R.usecase.account.AccountModifierUseCase;
 import com.StudyCafe_R.usecase.port.db.AccountPersistenceOperationsOutputPort;
 import com.StudyCafe_R.usecase.port.transaction.TransactionOperationsOutputPort;
 import com.StudyCafe_R.util.ClasspathAnonymousImageProvider;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
 public class UseCaseConfig {
